17543ede463eb7fb310ed1d56791dc48b81c17d7
Jay Wolberg <jmwolberg@gmail.com>
2026-03-03 21:27:23 -0800

    version 1.0

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import OptionsScanner from "./OptionsScanner.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <OptionsScanner />
+  </React.StrictMode>
+);
